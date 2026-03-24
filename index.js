9c038c8904786e0f96d4b452e377d768b2213c5f
trevorwinser <64448847+trevorwinser@users.noreply.github.com>
2026-03-24 16:57:33 -0700

    Update gateways | Add listener

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ const clientOptions = {
 	intents: [
 		GatewayIntentBits.Guilds,
 		GatewayIntentBits.GuildPresences,
-		GatewayIntentBits.GuildMembers
+		GatewayIntentBits.GuildMembers,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.MessageContent
 	],
 	allowedMentions: {
 		parse: ['users', 'roles', 'everyone']
@@ -25,6 +27,24 @@ const client = new Client(clientOptions);
 client.once(Events.ClientReady, (readyClient) => {
 	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
+
+client.on('messageCreate', async (message) => {
+  // 1. Ignore messages from bots (prevents infinite loops)
+  if (message.author.bot) return;
+
+  // 2. Filter by specific channel
+  if (message.channel.id !== '1485752622857326692' && message.channel.id !== '1486081968243474587') return;
+	console.log(`Received message: ${message.content}`);
+  try {
+    // 3. React! 
+    // Standard emoji: '✅'
+    // Custom emoji ID: '123456789012345678'
+    await message.react('1450675679128457307'); 
+  } catch (error) {
+    console.error('Failed to react:', error);
+  }
+});
+
 
 // Log in to Discord with your client's token
 const token = process.env.DISCORD_JUNO_TOKEN;
