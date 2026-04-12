18cf39f729b584761c97b48c3a356e67ac2ae3e7
Jon Klein <jonklein2021@gmail.com>
2026-04-12 18:14:58 -0400

    Add user util functions and use them in randomPing function

--- src/commands/fun/randomping.ts
+++ src/commands/fun/randomping.ts
@@ -1,30 +1,21 @@
-import { ChatInputCommandInteraction, Collection, SlashCommandBuilder } from 'discord.js';
+import { ChatInputCommandInteraction, MessageFlags, SlashCommandBuilder } from 'discord.js';
+import { getOnlineUsers } from '../../utils/users.ts';
 
 const data = new SlashCommandBuilder()
   .setName('randomping')
-  .setDescription('ping a random user in the server');
+  .setDescription('ping a random online user in the server');
 
-// pings a random online user in the server
 async function execute(interaction: ChatInputCommandInteraction) {
-  // First use guild.members.fetch to make sure all members are cached
-  const fetchedMembers =
-    (await interaction.guild?.members.fetch({ withPresences: true })) ?? new Collection();
-
-  // Filter the members to get only non-bots that are who are online
-  // N.B: Offline members have member.presence?.status === undefined
-  const onlineMembers = fetchedMembers
-    .filter((member) => !member.user.bot && member.presence?.status)
-    .mapValues((member) => member.user);
+  const onlineUsers = await getOnlineUsers(interaction);
 
   // pick a random member out of the total online members
-  const randomOnlineMemberId = onlineMembers.randomKey();
+  const randomOnlineMemberId = onlineUsers.randomKey();
 
-  // const randomOnlineMemberId = onlineMembers.random().user;
-  console.log(onlineMembers);
-
-  // quiet ping a random user
-  //await interaction.reply(`<@${randomOnlineMemberId}> meow`, { allowedMentions: { parse: [] } });
-  await interaction.reply(`<@${randomOnlineMemberId}> meow`);
+  // "quiet ping" a random user; they will not receive notifications
+  await interaction.reply({
+    content: `<@${randomOnlineMemberId}> meow`,
+    flags: MessageFlags.SuppressNotifications,
+  });
 }
 
 export { data, execute };
